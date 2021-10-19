18e0afc7037119d30c85966365956aae354aa727
seth.149586 <seth.149586@nv.ccsd.net>
2021-10-19 14:00:53 -0700

    // constuction tester changes

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -6,5 +6,36 @@ public class ConstructionTester
     public static void main(String[] args)
     {
         // Start here!
+        Scanner in = new Scanner(System.in);
+
+        // gets the tax rate
+        System.out.println("Enter the sales tax rate: ");
+        double tax = in.nextDouble();
+        System.out.println(tax);
+
+        //gets the number of boards
+        System.out.println("How many boards do you need?? ");
+        int numBoard = in.nextInt();
+        System.out.println(numBoard);
+
+        //gets the number of windows
+        System.out.println("How many windows do you need? ");
+        int numWindow = in.nextInt();
+        System.out.println(numWindow);
+
+        //creates a constuction object
+        Construction construct = new Construction(8, 11, tax);
+
+        //calculates cost of windows and boards
+        double bCost = construct.lumberCost(numBoard);
+        double wCost = construct.windowCost(numWindow);
+
+        //gets the total
+        double total = bCost + wCost;
+        System.out.println("Total: " + total);
+
+        //gets the total plus tax
+        double totalPlusTax = construct.grandTotal(total);
+        System.out.print("Grand total: " + totalPlusTax);
     }
 }
